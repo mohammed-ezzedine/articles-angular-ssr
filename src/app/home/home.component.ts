42f87dfc008e7455001b79ab16f4c98166db6147
Mohammed Ezzedine <mohammed.a.ezzedine@outlook.com>
2023-12-30 15:36:49 +0200

    Transferring state from the server to the browser

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, PLATFORM_ID, TransferState, makeStateKey } from '@angular/core';
 import { Article } from './article.model';
 import { Meta, Title } from '@angular/platform-browser';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -12,20 +13,42 @@ import { Meta, Title } from '@angular/platform-browser';
 })
 export class HomeComponent implements OnInit {
 
+  private readonly SERVER_DATA_KEY = makeStateKey<Article>("homeArticle");
+
   constructor(private http: HttpClient,
               private title: Title,
-              private meta: Meta) { }
+              private meta: Meta,
+              private transferState: TransferState,
+              @Inject(PLATFORM_ID) private platformId: Object) { }
 
   ngOnInit(): void {
+
+    if (isPlatformBrowser(this.platformId)) {
+      if (this.transferState.hasKey(this.SERVER_DATA_KEY)) {
+        let article = this.transferState.get<any>(this.SERVER_DATA_KEY, undefined);
+        this.initializeMetadata(article)
+        this.transferState.remove(this.SERVER_DATA_KEY);
+        return
+      }
+    }
+    
+
     this.http.get<Article>("https://mohammed.ezzedine.me/api/articles/QK7WwzbwbvX")
     .subscribe({
       next: data => {
-        this.title.setTitle(data.title);
-        this.meta.addTag({ name: "description", property: "og:decription", content: data.description })
-        this.meta.addTag({ name: "image", property: "og:image", content: data.imageUrl })
-        this.meta.addTag({ name: "keywords", content: data.keywords.reduce((k1, k2) => k1 + ", " + k2) })
+        if (isPlatformServer(this.platformId)) {
+          this.transferState.set(this.SERVER_DATA_KEY, data);
+        }
+        this.initializeMetadata(data)
       }
     })
+  }
+
+  initializeMetadata(article: Article) {
+    this.title.setTitle(article.title);
+    this.meta.addTag({ name: "description", property: "og:decription", content: article.description })
+    this.meta.addTag({ name: "image", property: "og:image", content: article.imageUrl })
+    this.meta.addTag({ name: "keywords", content: article.keywords.reduce((k1, k2) => k1 + ", " + k2) })
   }
 
 }
